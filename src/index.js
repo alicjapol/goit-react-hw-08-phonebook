34325087515740bdb45e70cbd0bf388f4c7d024c
alicjapol <alicjaxpoltorak@gmail.com>
2024-03-08 12:50:05 +0100

    Add redux-persist

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import {store} from './redux/store'
-
+import {store, persist} from './redux/store'
+import {PersistGate} from 'redux-persist/es/integration/react'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider
     store={store}>
+      <PersistGate loading={null} persistor={persist}>
     <App />
+    </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,22 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './auth/authSlice';
 import contactsReducer from './contactsSlice';
 import { devToolsEnhancer } from "@redux-devtools/extension";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer,persistStore } from 'redux-persist';
 
 const enhancer = devToolsEnhancer();
 
+const authPersistConfig = {
+  key: 'auth',
+  storage
+}
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
-    auth: authReducer, 
+    auth: persistReducer(authPersistConfig, authReducer), 
   },
 }, enhancer);
+
+
+export const persist = persistStore(store);
