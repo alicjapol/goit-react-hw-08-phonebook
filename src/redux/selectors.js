8cf8411688e68d888e90c6d20cab346e13028522
alicjapol <alicjaxpoltorak@gmail.com>
2024-03-09 16:18:08 +0100

    Use `createSelector` for `selectFilteredContacts`

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,10 +1,14 @@
+import { createSelector } from 'reselect';
+
 export const selectContacts = state => state.contacts.contacts;
 export const selectFilter = state => state.contacts.filter;
 
-export const selectFilteredContacts = state => {
-  const contacts = selectContacts(state);
-  const filter = selectFilter(state).toLowerCase();
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter)
-  );
-};
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    const lowerCaseFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(lowerCaseFilter)
+    );
+  }
+);
