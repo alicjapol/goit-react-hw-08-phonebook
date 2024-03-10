cdc478cfdc84238fb0060e6569355bcdc3e2ddca
alicjapol <alicjaxpoltorak@gmail.com>
2024-03-11 00:48:15 +0100

    Update avatar image source.

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -10,7 +10,7 @@ export default function UserMenu() {
   return (
     <div className="userMenuContainer">
     <div className="thumb-lg member-thumb mx-auto">
-      <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="profile-image" className='avatar' />
+      <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="avatar" className='avatar' />
     </div>
     <p className="userEmail">Email: {user?.email}</p>
     <LogoutButton />
