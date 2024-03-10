cbfc25d174ff48a0aa459ba4adb88f0f2d502407
alicjapol <alicjaxpoltorak@gmail.com>
2024-03-10 16:58:28 +0100

    Update login form styling

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,41 +1,41 @@
-
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from '../../redux/auth/operations';
-import {fetchContacts} from '../../redux/operations'
-function LoginForm() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchContacts());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      login({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
-  };
+import './LoginForm.css'; 
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Email:</label>
-        <input type="text" name="email" />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" name="password" />
-      </div>
-      <div>
-        <button type="submit">Login</button>
-      </div>
-    </form>
-  );
+function LoginForm() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        
+    }, [dispatch]);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        dispatch(
+            login({
+                email: form.elements.email.value,
+                password: form.elements.password.value,
+            })
+        );
+        form.reset();
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="loginForm">
+            <div>
+                <label>Email:</label>
+                <input type="text" name="email" />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type="password" name="password" />
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    );
 }
 
 export default LoginForm;
